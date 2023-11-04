f107be9975beac59066851a5c0ece48c88cbc2b1
RaniaG <raniagml12@gmail.com>
2023-11-04 16:51:54 +0200

    initial config

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  // webpackFinal: async (config) => {
+  //   return {
+  //     ...config,
+  //     module: {
+  //       ...config.module,
+  //       rules: [...config.module.rules, ...custom.module.rules],
+  //     },
+  //   };
+  // },
+};
+export default config;
